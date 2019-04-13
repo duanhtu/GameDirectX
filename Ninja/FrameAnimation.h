02615c95ac4e6b28505e9616d7a63640d2b292bf
DESKTOP-O8TUU0R\TU <daydreamer1804@gmail.com>
2019-04-13 16:33:22 +0700

    xóa project và add file .gitignore

--- Ninja/FrameAnimation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<Windows.h>
-/* Frame bao gom lop RECT va anchor X va anchor y */
-class FrameAnimation : public RECT
-{
-public:
-	int anchorX, anchorY;
-};
--- Ninja/GameUtils.h
+++
@@ -1,5 +0,0 @@
-/* Tien ich game, bao gom nhung phuong thuc tien ich giup lam game de hon */
-#include<fstream>
-using namespace std;
-/* bo qua dong chu khi doc file */
-extern void ignoreLineIfstream(ifstream& fs, int lineCount);
