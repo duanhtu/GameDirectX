d77deaef3c90eacdc6a9931b4ee51d7a5a93eeda
Tu Du Anh <duanhtu1804@gm.uit.edu.vn>
2019-05-17 11:23:06 +0700

    phan hoach khong gian bang grid nhung van loi

---
+++ Ninja/GridRect.h
@@ -0,0 +1,14 @@
+#include "Rect.h"
+#include"BaseObject.h"
+
+class GridRect :
+	public Rect
+{
+	List<BaseObject*> gridRectObjects;
+public:	
+	List<BaseObject*> getGridRectObjects();
+	static void addObjectToProperGridRect(List<GridRect*> allGridRects, BaseObject* object);
+	static List<BaseObject*> getCollisionObjects(List<GridRect*> allGridRects);
+	GridRect();
+	~GridRect();
+};
